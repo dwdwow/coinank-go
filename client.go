3f49ee9f8d04a86cfd6d9ec493b685a8a9feffc4
dwd <wenyi173@gmail.com>
2025-09-19 16:44:15 +0800

    update

---
+++ client.go
@@ -0,0 +1,15 @@
+package coinankgo
+
+type Client struct {
+	apiKey string
+}
+
+type ClientCfg struct {
+	ApiKey string
+}
+
+func NewClient(cfg ClientCfg) *Client {
+	return &Client{
+		apiKey: cfg.ApiKey,
+	}
+}
